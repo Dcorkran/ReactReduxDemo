10bfa3f443e954dae98f6563771619e5d9bc7981
Dillon Corkran <dcorkran972@gmail.com>
2017-03-18 11:08:07 -0600

    :sparkles: demo work

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,35 @@
+import React, {PropTypes} from 'react';
+
+
+class CoursesPage extends React.Component {
+  contructor(props, context){
+    super(props, context);
+
+    this.state = {
+      course: { title: null}
+    };
+  }
+
+  onClickSave(){
+    alert(`Saving ${this.state.course.title}`);
+  }
+  render() {
+    return (
+      <div>
+        <h1>Courses</h1>
+        <h2>Add Course</h2>
+        <input
+          type="text"
+          onChange={this.onTitleChange}
+          value={this.state.course.title} />
+
+          <input
+            type="submit"
+            value="Save"
+            onClick={this.onClickSave} />
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
